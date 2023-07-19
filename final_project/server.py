98bde1abca1c6710a5083e3232eec7d912143dae
tonethar <tonethar@users.noreply.github.com>
2023-07-19 18:57:48 -0400

    Task 6 Complete

--- final_project/server.py
+++ final_project/server.py
@@ -8,18 +8,24 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    #return "Translated text to French"
+    return translator.english_to_french(textToTranslate)
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    #return "Translated text to English"
+    return translator.french_to_english(textToTranslate)
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
-  beh = "book"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
+
+# http://127.0.0.1:8080/englishToFrench?textToTranslate=Hello
+# http://127.0.0.1:8080/frenchToEnglish?textToTranslate=Oui
